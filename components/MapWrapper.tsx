88ed6cb747eaebb58b4876bc0c36cf9b2389cd40
weiseng18 <tws.hci@gmail.com>
2024-09-30 09:27:53 +0800

    Setup basic idea

---
+++ components/MapWrapper.tsx
@@ -0,0 +1,33 @@
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { Point } from "../types/points";
+
+interface GoogleMapComponentProps {
+  defaultZoom: number;
+  defaultCenter: Point;
+  children?: React.ReactNode;
+}
+
+const containerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+
+const GoogleMapComponent = ({ defaultZoom, defaultCenter, children }: GoogleMapComponentProps) => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    // NOTE: It is necessary to expose the API key in the frontend in order to use the Google Maps API.
+    // The API key used here is restricted to only allow requests from my website.
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+  });
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={defaultCenter}
+      zoom={defaultZoom}
+    >{children}</GoogleMap>
+  ) : <></>;
+};
+
+export default GoogleMapComponent;
+
---
+++ components/TrainLine.tsx
@@ -0,0 +1,39 @@
+import { Circle, Polyline } from "@react-google-maps/api";
+import { Point } from "../types/points";
+
+interface TrainLineProps {
+  points: Point[];
+  color: string;
+}
+
+const TrainLine = ({ points, color }: TrainLineProps) => {
+  return <>
+    { points && points.length > 0 && (
+      <>
+        {points.map((point, index) => (
+          <Circle
+            key={index}
+            center={point}
+            radius={50}
+            options={{
+              fillColor: "white",
+              fillOpacity: 1,
+              strokeColor: "black",
+              strokeOpacity: 1,
+              strokeWeight: 3,
+            }}
+          />
+        ))}
+        <Polyline
+          path={points}
+          options={{
+            strokeColor: color,
+            strokeOpacity: 1,
+            strokeWeight: 5,
+          }}
+        />
+      </>
+    )}</>
+};
+
+export default TrainLine;
