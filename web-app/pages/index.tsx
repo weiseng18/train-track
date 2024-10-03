10fccc534e091dc6b6a1d2d632fc9881398ebe52
weiseng18 <tws.hci@gmail.com>
2024-10-03 13:10:32 +0800

    Add line colors constant

--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -3,15 +3,16 @@ import { Point } from "../types/points";
 import TrainLine from "../components/TrainLine";
 import { obtainLines } from "../utils/preprocessing";
 import { Line } from "../types/lines";
+import { lineColors } from "../constants/colors";
 
-const points: Point[] = obtainLines()[Line.Tozai].map((c) => ({
+const tozaiPoints: Point[] = obtainLines()[Line.Tozai].map((c) => ({
   lat: c.lat,
   lng: c.lng,
 }));
 
 const IndexPage = () => {
-  const defaultZoom = 15
-  const defaultCenter = points[3]
+  const defaultZoom = 15;
+  const defaultCenter = tozaiPoints[3];
 
   return (
     <div>
@@ -19,7 +20,7 @@ const IndexPage = () => {
         defaultZoom={defaultZoom}
         defaultCenter={defaultCenter}
       >
-        <TrainLine points={points} color={'#009BBF'} />
+        <TrainLine points={tozaiPoints} color={lineColors[Line.Tozai]} />
       </GoogleMapComponent>
     </div>
   )
