4ad08afe54b03dcd920443f8cb6f8e8ddbe7a0ff
weiseng18 <tws.hci@gmail.com>
2024-10-05 14:26:00 +0800

    Specify straight train lines

--- web-app/components/AllTrainLines.tsx
+++ web-app/components/AllTrainLines.tsx
@@ -1,23 +1,32 @@
 import { lineColors } from '../constants/colors'
+import { Line } from '../types/lines'
 import { obtainLines } from '../utils/preprocessing'
 import TrainLine from './TrainLine'
 
 const AllTrainLines = () => {
   const lineMap = obtainLines()
 
+  // These train lines are a straight line
+  const standardTrainLines = [
+    Line.Ginza,
+    Line.Hibiya,
+    Line.Tozai,
+    Line.Chiyoda,
+    Line.Yurakucho,
+    Line.Hanzomon,
+    Line.Namboku,
+    Line.Fukutoshin,
+  ]
+
   return (
     <div>
-      {Object.keys(lineMap)
-        .filter((line) => lineMap[line].length > 0) // Filter out empty lines
-        .map((line) => {
-          const points = lineMap[line].map((c) => ({
-            lat: c.lat,
-            lng: c.lng,
-          }))
-          return (
-            <TrainLine points={points} color={lineColors[line]} key={line} />
-          )
-        })}
+      {standardTrainLines.map((line) => {
+        const points = lineMap[line].map((c) => ({
+          lat: c.lat,
+          lng: c.lng,
+        }))
+        return <TrainLine points={points} color={lineColors[line]} key={line} />
+      })}
     </div>
   )
 }
